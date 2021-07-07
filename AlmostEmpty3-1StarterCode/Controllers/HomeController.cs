8345f2d0358ae00c022bdd4f1d659fb7edbd2e0a
Ryder <rdeback@gmail.com>
2021-07-07 12:48:57 -0700

    Got dynamic webpage titles.

--- AlmostEmpty3-1StarterCode/Controllers/HomeController.cs
+++ AlmostEmpty3-1StarterCode/Controllers/HomeController.cs
@@ -10,11 +10,14 @@ namespace AlmostEmpty3_1StarterCode.Controllers
     {
         public IActionResult Index()
         {
+            ViewBag.Title = "Home";
+            //ViewData["Title"] = "Home";
             return View();
         }
 
         public IActionResult About()
         {
+            ViewBag.Title = "About";
             return View();
         }
     }
